665f9a71e33baa6d857e6c2a4d3dcd16d5be96c8
weisshillscomma <alex@alex.weisshills.com>
2015-11-23 18:31:21 -0500

    many many updates

--- js/artek-flipbook.js
+++ js/artek-flipbook.js
@@ -1,78 +1,84 @@
-function positionDots () {
-	var dotsULWidth = $('.theDots ul').width();
-	var dotWidth = $('.theDots li.active').width();
-	var dotWidthPercent = (dotWidth/dotsULWidth)*100;
-	var dotsAfter = ($('.theDots li.active').nextAll('li').length) * 2;
-	var dotsPrev = $('.theDots li.active').prevAll('li').length;
-	var dotsAfterSpacing = 100/dotsAfter;
-	var dotsPrevSpacing = 50/dotsPrev;
-	$('.theDots li.active').animate({
-			left: '50%',
-	}, 1000);
-	var $a = 2;
-	var $p = 1;
-	$('.theDots li.active').prevAll('li').each(function(){
-		var thisSpacing = 50-(dotsPrevSpacing * $p);
-		$(this).animate({
-			left: thisSpacing + '%',
-		}, 1000);
-		$p++;
-	});
-	$('.theDots li.active').nextAll('li').each(function(){
-		var thisSpacing = dotsAfterSpacing * $a + 50 - dotWidthPercent;
-		$(this).animate({
-			left: thisSpacing + '%',
-		}, 1000);
-		$a++;
+function glow(that){
+	$(that).fadeOut(1000, function(){
+		$(that).fadeIn(1000, function(){
+			glow(that);
+		});
 	});
 }
+
 $(function(){
 	$('ul.imageGallery li:first-of-type').addClass('active');
 	$('.theDots li:first-of-type').addClass('active');
 	
 	$('.gallery_controls .prev').click(function(){
-		var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
+		// var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
 		if($('ul.imageGallery li.active').prev('li').length){
 			$('ul.imageGallery').animate({
-				left: '-=' + imageWidth
+				// left: '-=' + imageWidth
 			}, 300, function(){
 				$('ul.imageGallery li.active').removeClass('active').prev('li').addClass('active');
 				var theDot = ('.theDots #') + ($('ul.imageGallery li.active').attr('id'));
 				$('.theDots li.active').removeClass('active');
 				$(theDot).addClass('active');
-				positionDots();
+			});
+		} else {
+			$('ul.imageGallery').animate({
+				// left: '-=' + imageWidth
+			}, 300, function(){
+				$('ul.imageGallery li.active').removeClass('active');
+				$('ul.imageGallery li:last-of-type').addClass('active');
+				var theDot = ('.theDots #') + ($('ul.imageGallery li.active').attr('id'));
+				$('.theDots li.active').removeClass('active');
+				$(theDot).addClass('active');
 			});
 		}
 	});
 	$('.gallery_controls .next').click(function(){
-		var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
+		// var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
 		if($('ul.imageGallery li.active').next('li').length){
 			$('ul.imageGallery').animate({
-				left: '+=' + imageWidth
+				// left: '+=' + imageWidth
 			}, 300, function(){
 				$('ul.imageGallery li.active').removeClass('active').next('li').addClass('active');
 				var theDot = ('.theDots #') + ($('ul.imageGallery li.active').attr('id'));
 				$('.theDots li.active').removeClass('active');
 				$(theDot).addClass('active');
-				positionDots();
+			});
+		} else {
+			$('ul.imageGallery').animate({
+				// left: '-=' + imageWidth
+			}, 300, function(){
+				$('ul.imageGallery li.active').removeClass('active');
+				$('ul.imageGallery li:first-of-type').addClass('active');
+				var theDot = ('.theDots #') + ($('ul.imageGallery li.active').attr('id'));
+				$('.theDots li.active').removeClass('active');
+				$(theDot).addClass('active');
 			});
 		}
 	});
 
 	$('.theDots li').click(function(){
-		var index = $(this).index();
-		var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
-		var scrollTo = 	imageWidth * index;
+		// var index = $(this).index();
+		// var imageWidth = ($('ul.imageGallery li.active').width())*(-1);
+		// var scrollTo = 	imageWidth * index;
 		var theDot = '#' + $(this).attr('id');
 		$('.theDots li.active').removeClass('active');
 		$(this).addClass('active');
 		$('ul.imageGallery').animate({
-			left: scrollTo
+			// left: scrollTo
 		}, 300, function(){
 			$('ul.imageGallery li.active').removeClass('active');
 			$('ul.imageGallery').find(theDot).addClass('active');
 		});
-		positionDots();
 	});
-	positionDots();
+
+	$('.hotspot').each(function(){
+		glow(this);
+	});
+	$('.hotspot').click(function(){
+		$(this).next('.overlay').fadeIn(300);
+	});
+	$('.close').click(function(){
+		$(this).parent().parent().fadeOut(300);
+	});
 });
